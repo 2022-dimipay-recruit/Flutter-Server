bd29bbb7ff10a62c6328a38fc2f3c92495158671
H2Owater425 <h2o@h2owr.xyz>
2022-06-22 18:26:45 +0900

    fix: ensure social uids to be unique

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -3,8 +3,12 @@ import schema from 'fluent-json-schema';
 
 export default new Schema({
   id: schema.string().format('uuid'),
-  link: schema.string(),
-  nickname: schema.string(),
+  link: schema
+    .string()
+    .minLength(6)
+    .maxLength(16)
+    .pattern(/[a-z0-9]/),
+  nickname: schema.string().minLength(3).maxLength(20),
   email: schema.string().format('email'),
   profileImage: schema.string().format('url'),
   description: schema.string(),
