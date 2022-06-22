3da287949d2c7d48a2d93892eaf29a09109169b8
Tilto_ <tilto0822@gmail.com>
2022-06-22 15:45:40 +0900

    add: followController read function

--- src/controllers/FollowController.ts
+++ src/controllers/FollowController.ts
@@ -50,4 +50,40 @@ export default class FollowController {
       throw err;
     }
   }
+
+  // read follower by follwedId
+  public static async readFollower(client: PrismaClient, followedId: string) {
+    try {
+      const follows = await client.follows.findMany({
+        where: {
+          followingId: followedId,
+        },
+        include: {
+          follower: true,
+          following: true,
+        },
+      });
+      return follows;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  //read followed by followerId
+  public static async readFollowed(client: PrismaClient, followerId: string) {
+    try {
+      const follows = await client.follows.findMany({
+        where: {
+          followerId: followerId,
+        },
+        include: {
+          follower: true,
+          following: true,
+        },
+      });
+      return follows;
+    } catch (err) {
+      throw err;
+    }
+  }
 }
