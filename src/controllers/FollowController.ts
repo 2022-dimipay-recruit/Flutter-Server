be92da6ae49ea094abb2dfb71b61ab0930e8c7fa
Tilto_ <tilto0822@gmail.com>
2022-06-23 17:42:58 +0900

    [skip ci] doc: add follow controller jsdoc

--- src/controllers/FollowController.ts
+++ src/controllers/FollowController.ts
@@ -1,6 +1,13 @@
 import {PrismaClient} from '@prisma/client';
 
 export default class FollowController {
+  /**
+   * 팔로우를 생성하는 함수입니다.
+   * @param client PrismaClient를 넘깁니다.
+   * @param followerId 팔로우하는 사람의 id를 넘깁니다.
+   * @param followedId 팔로우받는 사람의 id를 넘깁니다.
+   * @returns 팔로우 정보를 넘깁니다.
+   */
   public static async createFollow(
     client: PrismaClient,
     followerId: string,
@@ -31,6 +38,14 @@ export default class FollowController {
     }
   }
 
+  /**
+   * 팔로우를 삭제하는 함수입니다.
+   * 팔로우를 만들 때 줬던 정보를 그대로 줘야 합니다.
+   * @param client PrismaClient를 넘깁니다.
+   * @param followerId 팔로우하는 사람의 id를 넘깁니다.
+   * @param followedId 팔로우받는 사람의 id를 넘깁니다.
+   * @returns 삭제된 팔로우의 정보를 넘깁니다.
+   */
   public static async deleteFollow(
     client: PrismaClient,
     followerId: string,
@@ -51,7 +66,13 @@ export default class FollowController {
     }
   }
 
-  // read follower by follwedId
+  /**
+   * 팔로우를 조회하는 함수입니다.
+   * 사용자의 ID를 넣으면 "그 사람을 팔로우한" 정보를 받습니다.
+   * @param client PrismaClient를 넘깁니다.
+   * @param followedId 팔로우받는 사람의 id를 넘깁니다.
+   * @returns 팔로우 정보 배열을 넘깁니다.
+   */
   public static async readFollower(client: PrismaClient, followedId: string) {
     try {
       const follows = await client.follows.findMany({
@@ -69,7 +90,13 @@ export default class FollowController {
     }
   }
 
-  //read followed by followerId
+  /**
+   * 팔로우를 조회하는 함수입니다.
+   * 사용자의 ID를 넣으면 "그 사람이 팔로우한" 정보를 받습니다.
+   * @param client PrismaClient를 넘깁니다.
+   * @param followerId 팔로우하는 사람의 id를 넘깁니다.
+   * @returns 팔로우 정보 배열을 넘깁니다.
+   */
   public static async readFollowed(client: PrismaClient, followerId: string) {
     try {
       const follows = await client.follows.findMany({
